e5c7073467385f5146df03a2c6da5bbdd951fb59
Taewapon <petchz2012@hotmail.com>
2019-08-23 03:27:17 +0700

    fully fuction register web

--- src/App.js
+++ src/App.js
@@ -1,81 +1,13 @@
 import React, { Component } from 'react';
-import { Button, Form } from "react-bootstrap";
 import './App.css';
-
-const liff = window.liff;
+import Register from './Register'
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayName : '',
-      userId : '',
-      pictureUrl : '',
-      statusMessage : ''
-    };
-    this.initialize = this.initialize.bind(this);
-    this.closeApp = this.closeApp.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener('load', this.initialize);
-  }
-
-  initialize() {
-    liff.init(async (data) => {
-      let profile = await liff.getProfile();
-      this.setState({
-        displayName : profile.displayName,
-        userId : profile.userId,
-        pictureUrl : profile.pictureUrl,
-        statusMessage : profile.statusMessage
-      });
-    }); 
-  }
-
-  closeApp(event) {
-    event.preventDefault();
-    liff.sendMessages([{
-      type: 'text',
-      text: "ลงทะเบียนเรียบร้อย"
-    }]).then(() => {
-      liff.closeWindow();
-    });
-  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-
-          <div className="">
-            {this.state.displayName}
-            <br></br>
-            {this.state.userID}
-          </div>
-
-          <div className="regis-form">
-            <Form>
-
-              <Form.Group>
-                <Form.Label>อายุคุณแม่ (ปี)</Form.Label>
-                <Form.Control type="number" placeholder="คุณแม่อายุกี่ปีคะ?" min="10" max="50" />
-              </Form.Group>
-
-              <Form.Group>
-                <Form.Label>อายุครรภ์ (สัปดาห์)</Form.Label>
-                <Form.Control type="number" placeholder="อายุครรภ์กี่สัปดาห์คะ?" min="1" max="50" />
-              </Form.Group>
-
-              <Button variant="danger" type="submit" onClick={this.closeApp}>
-                ยืนยัน
-            </Button>
-
-            </Form>
-
-          </div>
-        </header>
+        <Register/>
       </div>
     );
   }
---
+++ src/Register.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import { Button, Form } from "react-bootstrap";
+import './App.css';
+import axios from 'axios';
+
+const liff = window.liff;
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      line_id : '',
+      mom_age: '',
+      ges_age_week:''
+    };
+    this.initialize = this.initialize.bind(this);
+    this.closeApp = this.closeApp.bind(this);
+  }
+
+  initialize() {
+    liff.init(async () => {
+      let profile = await liff.getProfile();
+      this.setState({
+        line_id : profile.userId
+      });
+    }); 
+  }
+
+  componentDidMount() {
+    window.addEventListener('load', this.initialize);
+  }
+
+  closeApp(event) {
+    event.preventDefault();
+    liff.sendMessages([{
+      type: 'text',
+      text: "ลงทะเบียนเรียบร้อย"
+    }]).then(() => {
+      liff.closeWindow();
+    });
+  }
+
+  changeHandler = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  submitHandler = e => {
+    e.preventDefault()
+    console.log(this.state)
+    axios 
+    .post('https://0a197261.ngrok.io/register', this.state)
+    .then(response => {
+        console.log(response)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+  }
+
+  render() {
+    const {line_id, mom_age,ges_age_week} = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          {/* <div className="Show-data">
+            {this.state.line_id}
+          </div> */}
+
+          <div className="Regis-form">
+            <Form onSubmit={this.submitHandler}>
+              
+              <Form.Group>
+                <Form.Control
+                  className="hide"
+                  name="line_id"
+                  type="text"
+                  placeholder="Line ID"
+                  value={line_id}
+                  onChange={this.changeHandler} />
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label>อายุคุณแม่ (ปี)</Form.Label>
+                <Form.Control
+                  name="mom_age"
+                  type="number"
+                  placeholder="คุณแม่อายุกี่ปีคะ?"
+                  min="10"
+                  max="50"
+                  value={mom_age}
+                  onChange={this.changeHandler} />
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label>อายุครรภ์ (สัปดาห์)</Form.Label>
+                <Form.Control
+                  name="ges_age_week"
+                  type="number"
+                  placeholder="อายุครรภ์กี่สัปดาห์คะ?"
+                  min="1"
+                  max="50"
+                  value={ges_age_week}
+                  onChange={this.changeHandler} />
+              </Form.Group>
+
+              <Button variant="danger" type="submit" onClick={this.closeApp}>
+                ยืนยัน
+              </Button>
+
+            </Form>
+
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
